eca67f5046a5f071d010d86f50a1f21e1d4a9348
Ivan Solomennikov <van879@rambler.ru>
2023-04-11 00:57:27 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name;
+			int age;
+			string zodiac;
+			string workPlace;
+			Console.WriteLine("введите имя");
+			name = Console.ReadLine();
+			Console.WriteLine("ваш возраст");
+			age = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("ваш знак зодиака");
+			zodiac = Console.ReadLine();
+			Console.WriteLine("ваше место работы");
+			workPlace = Console.ReadLine();
+			Console.WriteLine($"Здравствуйте {name}, вам {age}, по знаку зодиака вы - {zodiac}, ваше место работы - {workPlace}");
+		}
+	}
+}
